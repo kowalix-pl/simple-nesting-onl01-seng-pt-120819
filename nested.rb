4f55e75f61b15620247bc4e77f2eccbc5872d59b
kowalix007 <kowalix007@gmail.com>
2019-12-19 21:48:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,6 @@ def adding_matz
         }
      }
 
-   
   programmer_hash << yukihiro_matsumoto 
 end
 
@@ -104,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:alans_new_info] = "GUI"
+     programmer_hash[:alan_kay][:known_for]["GUI"]
      
 end
 
